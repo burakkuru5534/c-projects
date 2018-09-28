5aa795a1c76a564f5d32bf2dbe57d046767f81ab
Burak Kuru <43616032+burakkuru5534@users.noreply.github.com>
2018-09-28 17:23:02 +0300

    Add files via upload

---
+++ Ascii.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <Windows.h>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+  for (int i=0;i<= 255;i++)
+  cout<< i<<" = "<< char(i)<< endl;
+ system("pause");
+ return 0;
+}	
