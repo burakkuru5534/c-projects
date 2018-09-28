4a23848cd891a35f3bfa55cf7d43bef0097b8bfb
Burak Kuru <43616032+burakkuru5534@users.noreply.github.com>
2018-09-28 17:13:49 +0300

    Add files via upload

---
+++ sinifim.cpp
@@ -0,0 +1,20 @@
+#include "sinifim.h"
+#include <iostream>
+using namespace std;
+
+Sinifim::Sinifim(int n, int s)
+:normal_deger(n),sabit_deger(s)
+{
+	cout<< normal_deger<<endl;
+	cout<<sabit_deger<<endl;
+}
+
+
+
+int main(){
+	
+	
+	Sinifim nesne1(15,16);
+int x;
+	cin>> x;
+}
---
+++ sinifim.h
@@ -0,0 +1,17 @@
+#ifndef SINIFIM_H
+#define SINIFIM_H
+
+class Sinifim
+{
+	
+	
+	public : 
+	Sinifim(int n,int s);
+		
+	
+	private : 
+	int normal_deger;
+	const int sabit_deger;
+};
+
+#endif
